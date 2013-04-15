229fc46ec12c62c7060898035f6f7e41460b86c7
Jan Peca <theboxercz@gmail.com>
2013-04-15 18:09:40 +0200

    Added english translation

---
+++ core/components/dailymenu/lexicon/en/default.inc.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Default Czech Lexicon Entries for DailyMenu
+ *
+ * @package dailymenu
+ * @subpackage lexicon
+ */
+$_lang['dailymenu'] = 'Daily Menu';
+$_lang['dailymenu.menu_desc'] = 'Manage yours daily menus.';
+$_lang['dailymenu.dishes'] = 'Dishes';
+$_lang['dailymenu.dish'] = 'Dish';
+$_lang['dailymenu.dish_create'] = 'Add dish';
+$_lang['dailymenu.dishes_create'] = 'Add more dishes';
+$_lang['dailymenu.dish_err_ae'] = 'Dish with this name already exists.';
+$_lang['dailymenu.dish_err_nf'] = 'Dish not found.';
+$_lang['dailymenu.dish_err_ns'] = 'Dish not specified.';
+$_lang['dailymenu.dishes_err_ns'] = 'Dishes not specified.';
+$_lang['dailymenu.price_err_ns'] = 'Price not specified.';
+$_lang['dailymenu.dish_err_remove'] = 'Dish can not be removed.';
+$_lang['dailymenu.dish_err_save'] = 'Dish can not be saved.';
+$_lang['dailymenu.dish_remove'] = 'Remove dish';
+$_lang['dailymenu.dish_remove_confirm'] = 'Do you really want to remove this dish?';
+$_lang['dailymenu.dish_update'] = 'Update dish';
+$_lang['dailymenu.intro_msg'] = 'Manage dishes.';
+$_lang['dailymenu.search...'] = 'Search...';
+$_lang['dailymenu.price'] = 'Price';
+$_lang['dailymenu.bold'] = 'Bold';
+$_lang['dailymenu.dishes_create_tooltip'] = 'Enter one dish per row.<br />Dish price enter at the end of the line.';
